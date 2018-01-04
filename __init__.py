9c3a3833271b6d6c987237f0a100b4d57962b9f8
nschwanke <n.schwanke24@gmail.com>
2018-01-04 17:10:44 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,55 @@
+# Copyright 2018 Noah Schwanke
+# This file is part of the MARCEL skill for the Clam Box Virtual Assistant
+from os.path import dirname
+
+import mycroft.util
+import time
+import requests
+import json
+import threading
+import sys
+from os.path import abspath, dirname
+sys.path.append(abspath(dirname(__file__)))
+import GPIO
+
+from adapt.intent import IntentBuilder
+
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'nschwanke'
+
+LOGGER = getLogger(__name__)
+
+class MarcelSkill(MycroftSkill):
+		def __init__(self):
+				super(MarcelSkill, self).__init__(name="MarcelSkill")
+
+		def initialize(self):
+				directions_bathroom_intent = IntentBuilder("DirectionsBathroomIntent"). \
+						require("DirectionsBathroomKeyword").build()
+				self.register_intent(directions_bathroom_intent, self.handle_directions_bathroom_intent)
+
+				# -------------------------------------------------------------------------------
+        
+def handle_directions_bathroom_intent(self, message):
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO3","On")
+				GPIO.set("GPIO4","Off")
+				time.sleep(1)
+				self.speak_dialog("directions.bathroom")
+				time.sleep(1) 				
+				GPIO.set("GPIO2","On")
+				GPIO.set("GPIO3","Off")
+
+				try:
+					start = time.time()
+					mycroft.util.wait_while_speaking()
+					end = time.time()
+					if (end - start) < 1:
+						time.sleep(10)
+				except:
+					time.sleep(10)
+
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO4","On")
