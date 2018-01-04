93daf13cc2e57a528220f39803109a2943cf7433
nschwanke <n.schwanke24@gmail.com>
2018-01-04 17:47:15 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,6 +31,11 @@ class MarcelSkill(MycroftSkill):
 				self.register_intent(directions_bathroom_intent, self.handle_directions_bathroom_intent)
 
 				# -------------------------------------------------------------------------------
+				hours_intent = IntentBuilder("HoursIntent"). \
+						require("HoursKeyword").build()
+				self.register_intent(hours_intent, self.handle_hours_intent)
+
+				# -------------------------------------------------------------------------------
         
 		def handle_directions_bathroom_intent(self, message):
 				GPIO.set("GPIO2","Off")
@@ -53,3 +58,26 @@ class MarcelSkill(MycroftSkill):
 
 				GPIO.set("GPIO2","Off")
 				GPIO.set("GPIO4","On")
+				
+		def handle_hours_intent(self, message):
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO3","On")
+				GPIO.set("GPIO4","Off")
+				time.sleep(1)
+				self.speak_dialog("hours")
+				time.sleep(1) 				
+				GPIO.set("GPIO2","On")
+				GPIO.set("GPIO3","Off")
+
+				try:
+					start = time.time()
+					mycroft.util.wait_while_speaking()
+					end = time.time()
+					if (end - start) < 1:
+						time.sleep(10)
+				except:
+					time.sleep(10)
+
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO4","On")
+
