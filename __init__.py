5d6ddd29bda08d85252ef967adbfa9bfccfcec5e
nschwanke <n.schwanke24@gmail.com>
2018-01-08 15:26:00 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -81,6 +81,11 @@ class MarcelSkill(MycroftSkill):
 				self.register_intent(menu_drinks_intent, self.handle_menu_drinks_intent)
 
 				# -------------------------------------------------------------------------------
+				location_intent = IntentBuilder("LocationIntent"). \
+						require("LocationKeyword").build()
+				self.register_intent(location_intent, self.handle_location_intent)
+
+				# -------------------------------------------------------------------------------
         
 		def handle_directions_bathroom_intent(self, message):
 				GPIO.set("GPIO2","Off")
@@ -324,6 +329,26 @@ class MarcelSkill(MycroftSkill):
 				GPIO.set("GPIO2","Off")
 				GPIO.set("GPIO4","On")
 
+		def handle_location_intent(self, message):
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO3","On")
+				GPIO.set("GPIO4","Off")
+				time.sleep(1)
+				self.speak_dialog("location")
+				time.sleep(1) 				
+				GPIO.set("GPIO2","On")
+				GPIO.set("GPIO3","Off")
 
+				try:
+					start = time.time()
+					mycroft.util.wait_while_speaking()
+					end = time.time()
+					if (end - start) < 1:
+						time.sleep(10)
+				except:
+					time.sleep(10)
+
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO4","On")
 
 
