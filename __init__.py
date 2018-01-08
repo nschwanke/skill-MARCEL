838127bc9cd2e1bdf7db209781061db242e58542
nschwanke <n.schwanke24@gmail.com>
2018-01-08 15:37:10 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -86,6 +86,11 @@ class MarcelSkill(MycroftSkill):
 				self.register_intent(location_intent, self.handle_location_intent)
 
 				# -------------------------------------------------------------------------------
+				menu_fish_intent = IntentBuilder("MenuFishIntent"). \
+						require("MenuFishKeyword").build()
+				self.register_intent(menu_fish_intent, self.handle_menu_fish_intent)
+
+				# -------------------------------------------------------------------------------
         
 		def handle_directions_bathroom_intent(self, message):
 				GPIO.set("GPIO2","Off")
@@ -350,5 +355,26 @@ class MarcelSkill(MycroftSkill):
 
 				GPIO.set("GPIO2","Off")
 				GPIO.set("GPIO4","On")
+		
+		def handle_menu_fish_intent(self, message):
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO3","On")
+				GPIO.set("GPIO4","Off")
+				time.sleep(1)
+				self.speak_dialog("menu.fish")
+				time.sleep(1) 				
+				GPIO.set("GPIO2","On")
+				GPIO.set("GPIO3","Off")
 
+				try:
+					start = time.time()
+					mycroft.util.wait_while_speaking()
+					end = time.time()
+					if (end - start) < 1:
+						time.sleep(10)
+				except:
+					time.sleep(10)
+
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO4","On")
 
