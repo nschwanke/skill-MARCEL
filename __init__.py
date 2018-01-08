229577f91aa1da8249b86c8ebdbf76a28a1f7fb4
nschwanke <n.schwanke24@gmail.com>
2018-01-08 15:53:02 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -91,6 +91,11 @@ class MarcelSkill(MycroftSkill):
 				self.register_intent(menu_fish_intent, self.handle_menu_fish_intent)
 
 				# -------------------------------------------------------------------------------
+				seasonal_intent = IntentBuilder("SeasonalIntent"). \
+						require("SeasonalKeyword").build()
+				self.register_intent(seasonal_intent, self.handle_seasonal_intent)
+
+				# -------------------------------------------------------------------------------
         
 		def handle_directions_bathroom_intent(self, message):
 				GPIO.set("GPIO2","Off")
@@ -378,3 +383,25 @@ class MarcelSkill(MycroftSkill):
 				GPIO.set("GPIO2","Off")
 				GPIO.set("GPIO4","On")
 
+		
+		def handle_seasonal_intent(self, message):
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO3","On")
+				GPIO.set("GPIO4","Off")
+				time.sleep(1)
+				self.speak_dialog("seasonal")
+				time.sleep(1) 				
+				GPIO.set("GPIO2","On")
+				GPIO.set("GPIO3","Off")
+
+				try:
+					start = time.time()
+					mycroft.util.wait_while_speaking()
+					end = time.time()
+					if (end - start) < 1:
+						time.sleep(10)
+				except:
+					time.sleep(10)
+
+				GPIO.set("GPIO2","Off")
+				GPIO.set("GPIO4","On")
